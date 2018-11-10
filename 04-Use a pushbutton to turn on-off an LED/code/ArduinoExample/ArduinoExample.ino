7dc757a86d2facc8d825738ad2cb9e2c0b32873f
Nguyen Duc Hoang <sunlight4d@gmail.com>
2018-11-10 17:55:12 +0700

    update

--- 04-Use a pushbutton to turn on-off an LED/code/ArduinoExample/ArduinoExample.ino
+++ 04-Use a pushbutton to turn on-off an LED/code/ArduinoExample/ArduinoExample.ino
@@ -4,22 +4,21 @@
  * Create push button
 */
 const int BUTTON_PIN = 2;//Pin2 connected to Button
-const int LED_PIN = 2;//Pin13 connected to a LED
-int currentButtonState;
+const int LED_PIN = 13;//Pin13 connected to a LED
+int currentButtonState = 0;
 
 void setup() {
   //run once  
   pinMode(BUTTON_PIN, INPUT);
-  pinMode(LED_PIN, OUTPUT);
+  pinMode(LED_PIN, OUTPUT);  
 }
 
 void loop() {  
   //Run repeatedly 
   currentButtonState = digitalRead(BUTTON_PIN);
   if(currentButtonState == HIGH) {
-  	digitalWrite(LED_PIN, HIGH);
+  	digitalWrite(LED_PIN, HIGH);    
   } else {
   	digitalWrite(LED_PIN, LOW);
   }
 }
-
