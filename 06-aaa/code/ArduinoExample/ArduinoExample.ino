c71f1f92ff1562ad685dcf79677906eb4de3471d
Nguyen Duc Hoang <sunlight4d@gmail.com>
2018-11-15 16:10:18 +0700

    update

---
+++ 06-aaa/code/ArduinoExample/ArduinoExample.ino
@@ -0,0 +1,14 @@
+/*
+ * Nguyen Duc Hoang - Programming tutorial channel
+ * https://www.youtube.com/c/nguyenduchoang
+ * Play a melody using tone() function 
+*/
+
+void setup() {  
+  //run once  
+  
+}
+
+void loop() {   
+  //Run repeatedly     
+}
