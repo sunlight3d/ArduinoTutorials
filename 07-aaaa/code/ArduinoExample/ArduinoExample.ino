8a5270c98954b585305b16ea3cac416edaf3de88
Nguyen Duc Hoang <sunlight4d@gmail.com>
2020-08-15 07:56:58 +0700

    update

--- 07-aaaa/code/ArduinoExample/ArduinoExample.ino
+++ 07-aaaa/code/ArduinoExample/ArduinoExample.ino
@@ -1,45 +1,60 @@
 #include <TM1637Display.h>
+#include "DHT.h"
 
 // Define the connections pins:
 #define CLK 2
 #define DIO 3
-int sensorPin = A0;// chân analog kết nối tới cảm biến LM35
-
-// Create display object of type TM1637Display:
+DHT dht(4, DHT11); //pin 4 connect to 
 TM1637Display display = TM1637Display(CLK, DIO);
-// Create degree Celsius symbol:
-const uint8_t celsius[] = {
-  SEG_A | SEG_B | SEG_F | SEG_G,  // Circle
-  SEG_A | SEG_D | SEG_E | SEG_F   // C
-};
 void setup() {
   Serial.begin(9600);
+  dht.begin();
   // Clear the display:
-  display.clear();
-  delay(1000);
+  display.clear();  
 }
 void loop() {
   // Set the brightness:
   display.setBrightness(0x0a);    
   int step = 0;
-  for(step = 5; step > 0; step--)  //Interrate NumStep
+  for(step = 2; step > 0; step--)  //Interrate NumStep
   {
     display.showNumberDec(step); //Display the Variable value;
     delay(500);  //A half second delay between steps.
-  }
-//  display.setSegments(celsius, 2, 2);//const uint8_t segments[], uint8_t length, uint8_t pos  
-  int temperature = getTemperature();
-  display.showNumberDec(temperature,true, 2, 0);
+  }  
+  measure();
   delay(5000);
   //display.clear();  
 }
-int getTemperature() {
-  float voltage = 0.0;
-  float temp = 0.0;  
-  while(temp < 10.0) {
-    int reading = analogRead(sensorPin); 
-    voltage = reading * 5.0 / 1024.0; 
-    temp = voltage * 100.0;    
-  }  
-  return (int)temp;
+void measure() {
+  float h = dht.readHumidity();
+  // Read temperature as Celsius (the default)
+  float t = dht.readTemperature();
+  // Read temperature as Fahrenheit (isFahrenheit = true)
+  float f = dht.readTemperature(true);
+
+  // Check if any reads failed and exit early (to try again).
+  if (isnan(h) || isnan(t) || isnan(f)) {
+    Serial.println("Failed to read from DHT sensor!");
+    return;
+  }
+
+  // Compute heat index in Fahrenheit (the default)
+  float hif = dht.computeHeatIndex(f, h);
+  // Compute heat index in Celsius (isFahreheit = false)
+  float hic = dht.computeHeatIndex(t, h, false);
+
+  Serial.print("Humidity: ");
+  Serial.print(h);
+  Serial.print(" %t");
+  Serial.print("Temperature: ");
+  display.showNumberDec(t,true, 2, 0);
+  Serial.print(t);
+  Serial.print(" *C ");
+  Serial.print(f);
+  Serial.print(" *Ft");
+  Serial.print("Heat index: ");
+  Serial.print(hic);
+  Serial.print(" *C ");
+  Serial.print(hif);
+  Serial.println(" *F");
 }
